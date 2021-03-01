55e24918deae36ddfdf8bb4a3ff381b95f6acf92
tcdude <tc@tizilogic.com>
2021-03-01 18:29:13 +0100

    Attempt to fix compile error

--- p4a-recipe/pyksolve/__init__.py
+++ p4a-recipe/pyksolve/__init__.py
@@ -15,14 +15,24 @@ class PyksolveRecipe(CppCompiledComponentsPythonRecipe):
 
     depends = ['python3', 'setuptools']
 
-    def build_compiled_components(self, arch):
-        self.setup_extra_args = ['-j', str(cpu_count())]
-        super(PyksolveRecipe, self).build_compiled_components(arch)
-        self.setup_extra_args = []
+    def get_recipe_env(self, arch=None, with_flags_in_cc=True):
+        env = super().get_recipe_env(arch, with_flags_in_cc)
+        if self.need_stl_shared:
+            env['CFLAGS'] += f' -I{self.stl_include_dir}'
+            env['CFLAGS'] += ' -frtti -fexceptions'
 
-    def rebuild_compiled_components(self, arch, env):
-        self.setup_extra_args = ['-j', str(cpu_count())]
-        super(PyksolveRecipe, self).rebuild_compiled_components(arch, env)
-        self.setup_extra_args = []
+            env['LDFLAGS'] += f' -L{self.get_stl_lib_dir(arch)}'
+            env['LDFLAGS'] += f' -l{self.stl_lib_name}'
+        return env
+
+#    def build_compiled_components(self, arch):
+#        self.setup_extra_args = ['-j', str(cpu_count())]
+#        super(PyksolveRecipe, self).build_compiled_components(arch)
+#        self.setup_extra_args = []
+#
+#    def rebuild_compiled_components(self, arch, env):
+#        self.setup_extra_args = ['-j', str(cpu_count())]
+#        super(PyksolveRecipe, self).rebuild_compiled_components(arch, env)
+#        self.setup_extra_args = []
 
 recipe = PyksolveRecipe()
